af53742f80ad5aff906e78401564924f3474630d
César <cert.tigre@gmail.com>
2021-10-29 03:59:26 -0600

    Diseño del Formlario de Ingreso de Datos

--- F_enter-data.Designer.cs
+++ F_enter-data.Designer.cs
@@ -28,19 +28,402 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.TxtFirstName = new System.Windows.Forms.TextBox();
+            this.TxtLastName = new System.Windows.Forms.TextBox();
+            this.TxtID = new System.Windows.Forms.TextBox();
+            this.TxtHouse = new System.Windows.Forms.TextBox();
+            this.TxtNeighborhood = new System.Windows.Forms.TextBox();
+            this.TxtCity = new System.Windows.Forms.TextBox();
+            this.TxtCountry = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.TxtPhone = new System.Windows.Forms.TextBox();
+            this.TxtWorkPlace = new System.Windows.Forms.TextBox();
+            this.TxtSalary = new System.Windows.Forms.TextBox();
+            this.TxtOtherIncome = new System.Windows.Forms.TextBox();
+            this.BtnAdd = new System.Windows.Forms.Button();
+            this.BtnNew = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.BtnNew);
+            this.groupBox1.Controls.Add(this.BtnAdd);
+            this.groupBox1.Controls.Add(this.TxtOtherIncome);
+            this.groupBox1.Controls.Add(this.TxtSalary);
+            this.groupBox1.Controls.Add(this.TxtWorkPlace);
+            this.groupBox1.Controls.Add(this.TxtPhone);
+            this.groupBox1.Controls.Add(this.comboBox3);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.TxtCountry);
+            this.groupBox1.Controls.Add(this.TxtCity);
+            this.groupBox1.Controls.Add(this.TxtNeighborhood);
+            this.groupBox1.Controls.Add(this.TxtHouse);
+            this.groupBox1.Controls.Add(this.TxtID);
+            this.groupBox1.Controls.Add(this.TxtLastName);
+            this.groupBox1.Controls.Add(this.TxtFirstName);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(458, 355);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos del Cliente";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre Completo:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "N° de DUI/ID:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Dirección:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Fecha de Nacimiento:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 211);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Teléfono:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 248);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Lugar de Trabajo:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 283);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Ingresos:";
+            // 
+            // TxtFirstName
+            // 
+            this.TxtFirstName.Location = new System.Drawing.Point(123, 23);
+            this.TxtFirstName.Name = "TxtFirstName";
+            this.TxtFirstName.Size = new System.Drawing.Size(150, 20);
+            this.TxtFirstName.TabIndex = 7;
+            // 
+            // TxtLastName
+            // 
+            this.TxtLastName.Location = new System.Drawing.Point(291, 23);
+            this.TxtLastName.Name = "TxtLastName";
+            this.TxtLastName.Size = new System.Drawing.Size(150, 20);
+            this.TxtLastName.TabIndex = 8;
+            // 
+            // TxtID
+            // 
+            this.TxtID.Location = new System.Drawing.Point(123, 59);
+            this.TxtID.Name = "TxtID";
+            this.TxtID.Size = new System.Drawing.Size(318, 20);
+            this.TxtID.TabIndex = 9;
+            // 
+            // TxtHouse
+            // 
+            this.TxtHouse.Location = new System.Drawing.Point(123, 95);
+            this.TxtHouse.Name = "TxtHouse";
+            this.TxtHouse.Size = new System.Drawing.Size(150, 20);
+            this.TxtHouse.TabIndex = 10;
+            // 
+            // TxtNeighborhood
+            // 
+            this.TxtNeighborhood.Location = new System.Drawing.Point(291, 95);
+            this.TxtNeighborhood.Name = "TxtNeighborhood";
+            this.TxtNeighborhood.Size = new System.Drawing.Size(150, 20);
+            this.TxtNeighborhood.TabIndex = 11;
+            // 
+            // TxtCity
+            // 
+            this.TxtCity.Location = new System.Drawing.Point(123, 134);
+            this.TxtCity.Name = "TxtCity";
+            this.TxtCity.Size = new System.Drawing.Size(150, 20);
+            this.TxtCity.TabIndex = 12;
+            // 
+            // TxtCountry
+            // 
+            this.TxtCountry.Location = new System.Drawing.Point(291, 134);
+            this.TxtCountry.Name = "TxtCountry";
+            this.TxtCountry.Size = new System.Drawing.Size(150, 20);
+            this.TxtCountry.TabIndex = 13;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(123, 171);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 21);
+            this.comboBox1.TabIndex = 14;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(229, 171);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(100, 21);
+            this.comboBox2.TabIndex = 15;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(335, 171);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(106, 21);
+            this.comboBox3.TabIndex = 16;
+            // 
+            // TxtPhone
+            // 
+            this.TxtPhone.Location = new System.Drawing.Point(123, 208);
+            this.TxtPhone.Name = "TxtPhone";
+            this.TxtPhone.Size = new System.Drawing.Size(318, 20);
+            this.TxtPhone.TabIndex = 17;
+            // 
+            // TxtWorkPlace
+            // 
+            this.TxtWorkPlace.Location = new System.Drawing.Point(123, 245);
+            this.TxtWorkPlace.Name = "TxtWorkPlace";
+            this.TxtWorkPlace.Size = new System.Drawing.Size(318, 20);
+            this.TxtWorkPlace.TabIndex = 18;
+            // 
+            // TxtSalary
+            // 
+            this.TxtSalary.Location = new System.Drawing.Point(123, 280);
+            this.TxtSalary.Name = "TxtSalary";
+            this.TxtSalary.Size = new System.Drawing.Size(150, 20);
+            this.TxtSalary.TabIndex = 19;
+            // 
+            // TxtOtherIncome
+            // 
+            this.TxtOtherIncome.Location = new System.Drawing.Point(291, 280);
+            this.TxtOtherIncome.Name = "TxtOtherIncome";
+            this.TxtOtherIncome.Size = new System.Drawing.Size(150, 20);
+            this.TxtOtherIncome.TabIndex = 20;
+            // 
+            // BtnAdd
+            // 
+            this.BtnAdd.Location = new System.Drawing.Point(229, 322);
+            this.BtnAdd.Name = "BtnAdd";
+            this.BtnAdd.Size = new System.Drawing.Size(100, 23);
+            this.BtnAdd.TabIndex = 21;
+            this.BtnAdd.Text = "Confirmar Datos";
+            this.BtnAdd.UseVisualStyleBackColor = true;
+            // 
+            // BtnNew
+            // 
+            this.BtnNew.Location = new System.Drawing.Point(341, 322);
+            this.BtnNew.Name = "BtnNew";
+            this.BtnNew.Size = new System.Drawing.Size(100, 23);
+            this.BtnNew.TabIndex = 22;
+            this.BtnNew.Text = "Nuevo Cliente";
+            this.BtnNew.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.comboBox4);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Location = new System.Drawing.Point(478, 13);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(296, 192);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Confirmación de Datos";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 26);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(95, 13);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Estado del Cliente:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(6, 66);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(142, 13);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "Tipo de Tarjetas disponibles:";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(154, 63);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 21);
+            this.comboBox4.TabIndex = 2;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(6, 111);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(131, 13);
+            this.label10.TabIndex = 3;
+            this.label10.Text = "Tasa de Interés Asignada:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(6, 157);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(94, 13);
+            this.label11.TabIndex = 4;
+            this.label11.Text = "Fecha de Emisión:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 27);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Abrir Tarjeta";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label12);
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Location = new System.Drawing.Point(478, 211);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(296, 157);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Apertura";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(7, 20);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(268, 52);
+            this.label12.TabIndex = 6;
+            this.label12.Text = "Al presionar este botón, el cliente acepta los términos y \r\ncondiciones impuestas" +
+    " según los datos que ha \r\nproporcionado al sistema y aceptó cumplir con los \r\npa" +
+    "gos respectivos de dicha tarjeta.";
+            this.label12.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // F_enter_data
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(786, 384);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "F_enter_data";
             this.Text = "Ingresar Datos";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtWorkPlace;
+        private System.Windows.Forms.TextBox TxtPhone;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox TxtCountry;
+        private System.Windows.Forms.TextBox TxtCity;
+        private System.Windows.Forms.TextBox TxtNeighborhood;
+        private System.Windows.Forms.TextBox TxtHouse;
+        private System.Windows.Forms.TextBox TxtID;
+        private System.Windows.Forms.TextBox TxtLastName;
+        private System.Windows.Forms.TextBox TxtFirstName;
+        private System.Windows.Forms.Button BtnAdd;
+        private System.Windows.Forms.TextBox TxtOtherIncome;
+        private System.Windows.Forms.TextBox TxtSalary;
+        private System.Windows.Forms.Button BtnNew;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label12;
     }
 }
