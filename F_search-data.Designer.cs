f235301f6d47e9d8921e4bdaaf32687ea2916aed
plink0e <pablolinqui@gmail.com>
2021-11-06 12:20:25 -0600

    Diseno Form search

--- F_search-data.Designer.cs
+++ F_search-data.Designer.cs
@@ -28,20 +28,188 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxNameSearch = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbxCardTypeSearch = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateFromSearch = new System.Windows.Forms.DateTimePicker();
+            this.dateToSearch = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cbxStatus = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(30, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // tbxNameSearch
+            // 
+            this.tbxNameSearch.Location = new System.Drawing.Point(33, 51);
+            this.tbxNameSearch.Name = "tbxNameSearch";
+            this.tbxNameSearch.Size = new System.Drawing.Size(256, 20);
+            this.tbxNameSearch.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(30, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tipo de tarjeta";
+            // 
+            // cbxCardTypeSearch
+            // 
+            this.cbxCardTypeSearch.FormattingEnabled = true;
+            this.cbxCardTypeSearch.Location = new System.Drawing.Point(33, 111);
+            this.cbxCardTypeSearch.Name = "cbxCardTypeSearch";
+            this.cbxCardTypeSearch.Size = new System.Drawing.Size(270, 21);
+            this.cbxCardTypeSearch.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(30, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Fecha de apertura";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(196, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Hasta";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(30, 170);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Desde";
+            // 
+            // dateFromSearch
+            // 
+            this.dateFromSearch.Location = new System.Drawing.Point(33, 190);
+            this.dateFromSearch.Name = "dateFromSearch";
+            this.dateFromSearch.Size = new System.Drawing.Size(136, 20);
+            this.dateFromSearch.TabIndex = 8;
+            // 
+            // dateToSearch
+            // 
+            this.dateToSearch.Location = new System.Drawing.Point(194, 190);
+            this.dateToSearch.Name = "dateToSearch";
+            this.dateToSearch.Size = new System.Drawing.Size(136, 20);
+            this.dateToSearch.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(36, 28);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(219, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Por favor, inserte un término de búsqueda";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbxStatus);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.dateToSearch);
+            this.groupBox1.Controls.Add(this.dateFromSearch);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.cbxCardTypeSearch);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.tbxNameSearch);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(28, 48);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(377, 323);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(311, 377);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(94, 33);
+            this.btnSearch.TabIndex = 12;
+            this.btnSearch.Text = "Buscar";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(28, 377);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(94, 33);
+            this.btnClear.TabIndex = 13;
+            this.btnClear.Text = "Limpiar todo";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(30, 230);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(107, 17);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Estado";
+            // 
+            // cbxStatus
+            // 
+            this.cbxStatus.FormattingEnabled = true;
+            this.cbxStatus.Location = new System.Drawing.Point(33, 260);
+            this.cbxStatus.Name = "cbxStatus";
+            this.cbxStatus.Size = new System.Drawing.Size(270, 21);
+            this.cbxStatus.TabIndex = 11;
             // 
             // F_search_data
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(435, 435);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label6);
             this.Name = "F_search_data";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Datos de Búsqueda";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxNameSearch;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbxCardTypeSearch;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateFromSearch;
+        private System.Windows.Forms.DateTimePicker dateToSearch;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cbxStatus;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnClear;
     }
 }
