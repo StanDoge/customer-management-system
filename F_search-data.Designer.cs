14bb3997440f895af28dfe3bbdf731266193781b
plink0e <pablolinqui@gmail.com>
2021-11-07 14:46:22 -0600

    validacion de fechas y labels de rango form search

--- F_search-data.Designer.cs
+++ F_search-data.Designer.cs
@@ -38,15 +38,15 @@
             this.btnClear = new System.Windows.Forms.Button();
             this.Volver = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.btnGet = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.lblCount = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.lblFrom = new System.Windows.Forms.Label();
             this.lblTo = new System.Windows.Forms.Label();
+            this.lblFrom = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -54,7 +54,7 @@
             // 
             // label3
             // 
-            this.label3.Location = new System.Drawing.Point(36, 27);
+            this.label3.Location = new System.Drawing.Point(113, 29);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(107, 17);
             this.label3.TabIndex = 5;
@@ -62,7 +62,7 @@
             // 
             // label4
             // 
-            this.label4.Location = new System.Drawing.Point(441, 56);
+            this.label4.Location = new System.Drawing.Point(395, 56);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(176, 17);
             this.label4.TabIndex = 6;
@@ -71,7 +71,7 @@
             // 
             // label5
             // 
-            this.label5.Location = new System.Drawing.Point(136, 56);
+            this.label5.Location = new System.Drawing.Point(234, 56);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(107, 17);
             this.label5.TabIndex = 7;
@@ -80,20 +80,24 @@
             // 
             // dtpFromSearch
             // 
-            this.dtpFromSearch.Location = new System.Drawing.Point(92, 76);
+            this.dtpFromSearch.AllowDrop = true;
+            this.dtpFromSearch.CustomFormat = "";
+            this.dtpFromSearch.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFromSearch.Location = new System.Drawing.Point(233, 76);
             this.dtpFromSearch.Name = "dtpFromSearch";
-            this.dtpFromSearch.Size = new System.Drawing.Size(215, 20);
+            this.dtpFromSearch.Size = new System.Drawing.Size(108, 20);
             this.dtpFromSearch.TabIndex = 8;
+            this.dtpFromSearch.TabStop = false;
             this.dtpFromSearch.ValueChanged += new System.EventHandler(this.dtpFromSearch_ValueChanged);
             // 
             // dtpToSearch
             // 
             this.dtpToSearch.AllowDrop = true;
-            this.dtpToSearch.Location = new System.Drawing.Point(422, 76);
+            this.dtpToSearch.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpToSearch.Location = new System.Drawing.Point(429, 76);
             this.dtpToSearch.Name = "dtpToSearch";
-            this.dtpToSearch.Size = new System.Drawing.Size(215, 20);
+            this.dtpToSearch.Size = new System.Drawing.Size(108, 20);
             this.dtpToSearch.TabIndex = 9;
-            this.dtpToSearch.CloseUp += new System.EventHandler(this.dtpToSearch_CloseUp);
             this.dtpToSearch.ValueChanged += new System.EventHandler(this.dtpToSearch_ValueChanged);
             // 
             // groupBox1
@@ -103,7 +107,7 @@
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Location = new System.Drawing.Point(30, 589);
+            this.groupBox1.Location = new System.Drawing.Point(30, 448);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(768, 122);
             this.groupBox1.TabIndex = 11;
@@ -111,7 +115,7 @@
             // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(704, 717);
+            this.btnSearch.Location = new System.Drawing.Point(704, 576);
             this.btnSearch.Name = "btnSearch";
             this.btnSearch.Size = new System.Drawing.Size(94, 33);
             this.btnSearch.TabIndex = 12;
@@ -122,7 +126,7 @@
             // btnClear
             // 
             this.btnClear.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.btnClear.Location = new System.Drawing.Point(365, 717);
+            this.btnClear.Location = new System.Drawing.Point(365, 576);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(94, 33);
             this.btnClear.TabIndex = 13;
@@ -133,7 +137,7 @@
             // Volver
             // 
             this.Volver.BackColor = System.Drawing.SystemColors.Menu;
-            this.Volver.Location = new System.Drawing.Point(30, 717);
+            this.Volver.Location = new System.Drawing.Point(30, 576);
             this.Volver.Name = "Volver";
             this.Volver.Size = new System.Drawing.Size(94, 33);
             this.Volver.TabIndex = 14;
@@ -154,95 +158,95 @@
             this.groupBox2.Controls.Add(this.dataGridView1);
             this.groupBox2.Location = new System.Drawing.Point(30, 18);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(765, 565);
+            this.groupBox2.Size = new System.Drawing.Size(765, 424);
             this.groupBox2.TabIndex = 15;
             this.groupBox2.TabStop = false;
             // 
-            // dataGridView1
+            // lblTo
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(6, 23);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(753, 386);
-            this.dataGridView1.TabIndex = 0;
+            this.lblTo.AutoSize = true;
+            this.lblTo.Location = new System.Drawing.Point(556, 385);
+            this.lblTo.Name = "lblTo";
+            this.lblTo.Size = new System.Drawing.Size(35, 13);
+            this.lblTo.TabIndex = 8;
+            this.lblTo.Text = "label9";
+            // 
+            // lblFrom
+            // 
+            this.lblFrom.AutoSize = true;
+            this.lblFrom.Location = new System.Drawing.Point(556, 334);
+            this.lblFrom.Name = "lblFrom";
+            this.lblFrom.Size = new System.Drawing.Size(35, 13);
+            this.lblFrom.TabIndex = 7;
+            this.lblFrom.Text = "label8";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(487, 385);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Hasta:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(487, 334);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Desde:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(487, 291);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Búsqueda actual";
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(362, 353);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(35, 13);
+            this.lblCount.TabIndex = 3;
+            this.lblCount.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(176, 353);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Cantidad de aperturas mostradas:";
             // 
             // btnGet
             // 
-            this.btnGet.Location = new System.Drawing.Point(6, 526);
+            this.btnGet.Location = new System.Drawing.Point(42, 343);
             this.btnGet.Name = "btnGet";
             this.btnGet.Size = new System.Drawing.Size(101, 33);
             this.btnGet.TabIndex = 1;
             this.btnGet.Text = "Obtener";
             this.btnGet.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // dataGridView1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(146, 494);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(165, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Cantidad de aperturas mostradas:";
-            // 
-            // lblCount
-            // 
-            this.lblCount.AutoSize = true;
-            this.lblCount.Location = new System.Drawing.Point(332, 494);
-            this.lblCount.Name = "lblCount";
-            this.lblCount.Size = new System.Drawing.Size(35, 13);
-            this.lblCount.TabIndex = 3;
-            this.lblCount.Text = "label2";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(457, 432);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(87, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Búsqueda actual";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(457, 475);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(41, 13);
-            this.label6.TabIndex = 5;
-            this.label6.Text = "Desde:";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(457, 526);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(38, 13);
-            this.label7.TabIndex = 6;
-            this.label7.Text = "Hasta:";
-            // 
-            // lblFrom
-            // 
-            this.lblFrom.AutoSize = true;
-            this.lblFrom.Location = new System.Drawing.Point(526, 475);
-            this.lblFrom.Name = "lblFrom";
-            this.lblFrom.Size = new System.Drawing.Size(35, 13);
-            this.lblFrom.TabIndex = 7;
-            this.lblFrom.Text = "label8";
-            // 
-            // lblTo
-            // 
-            this.lblTo.AutoSize = true;
-            this.lblTo.Location = new System.Drawing.Point(526, 526);
-            this.lblTo.Name = "lblTo";
-            this.lblTo.Size = new System.Drawing.Size(35, 13);
-            this.lblTo.TabIndex = 8;
-            this.lblTo.Text = "label9";
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 23);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(753, 243);
+            this.dataGridView1.TabIndex = 0;
             // 
             // F_search_data
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(831, 767);
+            this.ClientSize = new System.Drawing.Size(831, 622);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.Volver);
             this.Controls.Add(this.btnClear);
--- F_search-data.cs
+++ F_search-data.cs
@@ -15,56 +15,106 @@ namespace Clave5_Grupo9
         public F_search_data()
         {
             InitializeComponent();
+           
+
         }
 
+        string dateFromSearch;
+        string dateToSearch;
+        string displayFrom;
+        string displayTo;
 
-        string nameSearch ="";
-        string cardTypeSearch ="";
-        string statusSearch="";
-        string dateFromSearch="";
-        string dateToSearch="";
+
 
         //Los botones no se activan hasta que haya algun dato insertado
         private void F_search_data_Load_1(object sender, EventArgs e)
         {
             btnSearch.Enabled = false;
             btnClear.Enabled = false;
+            dtpFromSearch.Value = DateTime.Now;
+            dtpToSearch.Value = dtpFromSearch.Value.AddDays(1);
+
+
+
+
         }
 
-
+        //fechas en formato ISO 
         private void dtpFromSearch_ValueChanged(object sender, EventArgs e)
         {
-            dateFromSearch = dtpFromSearch.Value.Date.ToShortTimeString();
+            dateFromSearch = dtpFromSearch.Value.Date.ToString("yyyy-MM-dd");
 
             btnSearch.Enabled = true;
             btnClear.Enabled = true;
 
         }
+
+
+        //Valida que el rango de fechas sea correcto
+        void validDate()
+        {
+            
+            
+            DateTime a = Convert.ToDateTime(dtpFromSearch.Text);
+            DateTime b = Convert.ToDateTime(dtpToSearch.Text);
+
+
+           
+            if (a > b)
+            {
+                MessageBox.Show("La fecha \"Hasta\" debe ser mayor que \"Desde\"", "Rango de fecha incorrecto",
+                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            
+        }
+
+
+
+
+
+
 
         private void dtpToSearch_ValueChanged(object sender, EventArgs e)
         {
-           
 
-            dateToSearch = dtpToSearch.Value.Date.ToShortTimeString();
+
+            dateToSearch = dtpToSearch.Value.Date.ToString("yyyy-MM-dd");
 
             btnSearch.Enabled = true;
             btnClear.Enabled = true;
+
+            DateTime a = Convert.ToDateTime(dtpFromSearch.Text);
+            DateTime b = Convert.ToDateTime(dtpToSearch.Text);
+            if (b < a)
+            {
+                MessageBox.Show("La fecha \"Desde\" debe ser menor que \"Hasta\"", "Rango de fecha incorrecto",
+                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
+            
 
-
-        //valida que haya al menos un campo con datos
+        //valida que haya al menos un campo con datos antes de buscar
         private void btnSearch_Click(object sender, EventArgs e)
         {
-            if (nameSearch
-             + cardTypeSearch
-             + statusSearch
-             + dateFromSearch
+            if (
+               dateFromSearch
              + dateToSearch == "")
             {
                 MessageBox.Show("Por favor ingrese un valor", "Campos vacios",
                 MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
+            else
+            {
+                displayFrom = dtpFromSearch.Value.Date.ToString("dd/MM/yyy");
+                displayTo = dtpToSearch.Value.Date.ToString("dd/MM/yyy");
+                lblFrom.Text = displayFrom;
+                lblTo.Text = displayTo;
+
+                validDate();
+
+            }
         }
+
 
 
         //limpiar todos los campos
@@ -80,26 +130,10 @@ namespace Clave5_Grupo9
             Hide();
         }
 
-
-        //Valida que el rango de fechas sea correcto
-        private void dtpToSearch_CloseUp(object sender, EventArgs e)
+        private void label3_Click(object sender, EventArgs e)
         {
-            DateTime a = Convert.ToDateTime(dtpFromSearch.Text);
-            DateTime b = Convert.ToDateTime(dtpToSearch.Text);
 
-            if (a >=b)
-            {
-                MessageBox.Show("La fecha \"Hasta\" debe ser mayor que \"Desde\"", "Rango de fecha incorrecto",
-                MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-            else
-            {
-                if (a <= b)
-                {
-                    MessageBox.Show("La fecha \"Desde\" debe ser menor que \"Hasta\"", "Rango de fecha incorrecto",
-                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                }
-            }
         }
     }
+    
 }
