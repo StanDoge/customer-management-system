7cee2bc24e500169e6ff2a855f6c8325f4390837
stanley_R <StanlyDoge@gmail.com>
2021-11-09 13:39:43 -0600

    Solucion del desplazamiento de bloques de codigo

--- F_enter-data.cs
+++ F_enter-data.cs
@@ -21,74 +21,73 @@ namespace Clave5_Grupo9
     }
 
 
-        string limit;
-        private void BtnConfirm_Click(object sender, EventArgs e)
+    string limit;
+    private void BtnConfirm_Click(object sender, EventArgs e)
     {
 
-    //asignacion de la informacion de los input al objeto cliente
+      //asignacion de la informacion de los input al objeto cliente
 
-    defaultCustomer.fullName = TbFirstName.Text + TbLastName.Text;
-    defaultCustomer.address = TbHouse.Text + TbNeighborhood.Text + TbCity.Text;
-    defaultCustomer.birthday = DtpDateOfBirth.Value.Date.ToString("yyyy-MM-dd");
-            defaultCustomer.workPlace = TbWorkPlace.Text;
-    defaultCustomer.DUI = Convert.ToInt32(TbID.Text);
+      defaultCustomer.fullName = TbFirstName.Text + TbLastName.Text;
+      defaultCustomer.address = TbHouse.Text + TbNeighborhood.Text + TbCity.Text;
+      defaultCustomer.birthday = DtpDateOfBirth.Value.Date.ToString("yyyy-MM-dd");
+      defaultCustomer.workPlace = TbWorkPlace.Text;
+      defaultCustomer.DUI = Convert.ToInt32(TbID.Text);
 
 
+      //Validación de entradas en los textbox para impedir que queden vacios y donde deban ir números solo acepte números.
+      bool number = false;//Variable que permitirá donde deban ir solamente números
+      double addInterest;
 
-            //Validación de entradas en los textbox para impedir que queden vacios y donde deban ir números solo acepte números.
-            bool number = false;//Variable que permitirá donde deban ir solamente números
-    double addInterest;   
-            
-      if (string.IsNullOrEmpty(defaultCustomer.fullName)) 
+      if (string.IsNullOrEmpty(defaultCustomer.fullName))
       {
         MessageBox.Show("Debe insertar el nombre completo");
         return;
       }
 
       number = int.TryParse(TbID.Text, out defaultCustomer.DUI);
-      if (!number || string.IsNullOrEmpty(TbID.Text))  
+      if (!number || string.IsNullOrEmpty(TbID.Text))
       {
         MessageBox.Show("El dato ingresado en DUI/ID no es válido o el campo está vacío");
         return;
       }
-      
 
-      if (string.IsNullOrEmpty(defaultCustomer.address)) 
+
+      if (string.IsNullOrEmpty(defaultCustomer.address))
       {
         MessageBox.Show("Debe ingresar su dirección completa");
         return;
       }
 
-      if (string.IsNullOrEmpty(defaultCustomer.birthday))  
+      if (string.IsNullOrEmpty(defaultCustomer.birthday))
       {
         MessageBox.Show("No ha ingresado una fecha de nacimiento");
         return;
       }
 
       number = int.TryParse(TbPhone.Text, out defaultCustomer.phoneNumber);
-      if (!number || string.IsNullOrEmpty(TbPhone.Text)) 
+      if (!number || string.IsNullOrEmpty(TbPhone.Text))
       {
         MessageBox.Show("El Teléfono no es válido o el campo está vacío");
         return;
       }
       defaultCustomer.phoneNumber = int.Parse(TbPhone.Text);
 
-      if (string.IsNullOrEmpty(defaultCustomer.workPlace))  
+      if (string.IsNullOrEmpty(defaultCustomer.workPlace))
       {
         MessageBox.Show("No ha ingresado un lugar de trabajo");
         return;
       }
 
-            
+
       number = double.TryParse(TbIncome.Text, out defaultCustomer.totalIncome);
-      if (!number || string.IsNullOrEmpty(TbIncome.Text)) 
+      if (!number || string.IsNullOrEmpty(TbIncome.Text))
       {
         MessageBox.Show("No ha ingresado un sueldo o ingreso principal");
         return;
       }
 
       number = double.TryParse(TbOtherIncome.Text, out defaultCustomer.totalIncome);
-      if (!number || string.IsNullOrEmpty(TbOtherIncome.Text))  
+      if (!number || string.IsNullOrEmpty(TbOtherIncome.Text))
       {
         MessageBox.Show("El dato ingresado en Otros Ingresos no es válido o el campo está vacío. Ingrese el número cero si no posee otros ingresos");
         return;
@@ -96,15 +95,15 @@ namespace Clave5_Grupo9
       defaultCustomer.totalIncome = double.Parse(TbIncome.Text) + double.Parse(TbOtherIncome.Text);
 
       number = double.TryParse(TbInterestRate.Text, out addInterest);
-      if (!number || string.IsNullOrEmpty(TbInterestRate.Text) || Convert.ToDouble(TbInterestRate.Text) < 0.30 || Convert.ToDouble(TbInterestRate.Text) > 0.40)  
+      if (!number || string.IsNullOrEmpty(TbInterestRate.Text) || Convert.ToDouble(TbInterestRate.Text) < 0.30 || Convert.ToDouble(TbInterestRate.Text) > 0.40)
       {
         MessageBox.Show("La tasa de interés no es válida o el campo se encuntra vacío");
         return;
       }
 
-       int indexOfSelections = CbCardsTypes.SelectedIndex;
-       
-            //comprobacion de que es apto para el tipo de tarjeta elegido
+      int indexOfSelections = CbCardsTypes.SelectedIndex;
+
+      //comprobacion de que es apto para el tipo de tarjeta elegido
       switch (indexOfSelections)
       {
         case 0:
@@ -150,86 +149,81 @@ namespace Clave5_Grupo9
           }
           break;
       }
+      double ingresoTotal;
+      ingresoTotal = Convert.ToDouble(TbIncome.Text) + Convert.ToDouble(TbOtherIncome.Text);
+
+      F_Confirm.nombre = defaultCustomer.fullName;
+      F_Confirm.dui = Convert.ToInt32(TbID.Text);
+      F_Confirm.direccion = defaultCustomer.address;
+      F_Confirm.fechanacimiento = defaultCustomer.birthday;
+      F_Confirm.tel = Convert.ToInt32(TbPhone.Text);
+      F_Confirm.trabajo = defaultCustomer.workPlace;
+      F_Confirm.ingresos = ingresoTotal;
+      F_Confirm.estado = LblStatus.Text;
+      F_Confirm.tipotarj = CbCardsTypes.Text;
+      F_Confirm.tarjlim = Convert.ToDouble(limit);
+      F_Confirm.interes = Convert.ToDouble(TbInterestRate.Text);
+
+      F_Confirm confirmar = new F_Confirm();
+      confirmar.Show();
 
     }
 
-        private void BtnNew_Click(object sender, EventArgs e)
-        {
-            TbFirstName.ResetText();
-            TbLastName.ResetText();
-            TbID.Clear();
-            TbHouse.ResetText();
-            TbNeighborhood.ResetText();
-            TbCity.ResetText();
-            DtpDateOfBirth.ResetText();
-            TbPhone.Clear();
-            TbWorkPlace.ResetText();
-            TbIncome.ResetText();
-            TbOtherIncome.ResetText();
-            CbCardsTypes.ResetText();
-            TbInterestRate.Clear();
-            LblStatus.ResetText();
-        }
+    private void BtnNew_Click(object sender, EventArgs e)
+    {
+      TbFirstName.ResetText();
+      TbLastName.ResetText();
+      TbID.Clear();
+      TbHouse.ResetText();
+      TbNeighborhood.ResetText();
+      TbCity.ResetText();
+      DtpDateOfBirth.ResetText();
+      TbPhone.Clear();
+      TbWorkPlace.ResetText();
+      TbIncome.ResetText();
+      TbOtherIncome.ResetText();
+      CbCardsTypes.ResetText();
+      TbInterestRate.Clear();
+      LblStatus.ResetText();
     }
-            double ingresoTotal;
-            ingresoTotal = Convert.ToDouble(TbIncome.Text) + Convert.ToDouble(TbOtherIncome.Text);
-
-            F_Confirm.nombre = defaultCustomer.fullName;
-            F_Confirm.dui = Convert.ToInt32(TbID.Text);
-            F_Confirm.direccion = defaultCustomer.address;
-            F_Confirm.fechanacimiento = defaultCustomer.birthday;
-            F_Confirm.tel = Convert.ToInt32(TbPhone.Text);
-            F_Confirm.trabajo = defaultCustomer.workPlace;
-            F_Confirm.ingresos = ingresoTotal;
-            F_Confirm.estado = LblStatus.Text;
-            F_Confirm.tipotarj = CbCardsTypes.Text;
-            F_Confirm.tarjlim = Convert.ToDouble(limit);
-            F_Confirm.interes = Convert.ToDouble(TbInterestRate.Text);
-
-            F_Confirm confirmar = new F_Confirm();
-            confirmar.Show();
-
-            
-           
-
-        }
-        
 
 
-        
-        public void btnClear_Click(object sender, EventArgs e)
-            
+    public void btnClear_Click(object sender, EventArgs e)
+
+    {
+      foreach (Control ctrl in groupBox1.Controls)
+
+      {
+
+        if (ctrl is TextBox)
+
         {
-            foreach (Control ctrl in groupBox1.Controls)
 
-            {
+          TextBox textBox = (TextBox)ctrl;
 
-                if (ctrl is TextBox)
-
-                {
-
-                    TextBox textBox = (TextBox)ctrl;
-
-                    textBox.Text = null;
-
-                }
-
-                if (ctrl is ComboBox)
-
-                {
-
-                    ComboBox comboBox = (ComboBox)ctrl;
-
-                    comboBox.SelectedIndex = -1;
-                }
-            }
-
-
+          textBox.Text = null;
 
         }
 
+        if (ctrl is ComboBox)
 
-       
+        {
+
+          ComboBox comboBox = (ComboBox)ctrl;
+
+          comboBox.SelectedIndex = -1;
+        }
+      }
+
+    }
+
+
 
   }
+
+
 }
+
+
+
+
